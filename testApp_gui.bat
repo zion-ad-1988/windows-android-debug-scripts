31648281f93d6c6b79e001f5a9c06455983d7a55
zion-ad-1988 <zion.ad.1988@gmail.com>
2026-09-22 17:17:07 +0800

    fix: hide PowerShell console when launching GUI

--- testApp_gui.bat
+++ testApp_gui.bat
@@ -1,4 +1,4 @@
 @echo off
 setlocal
 set "PATH=%~dp0scrcpy;%PATH%"
-start "" powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0testApp_gui.ps1"
+start "" powershell -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "%~dp0testApp_gui.ps1"
